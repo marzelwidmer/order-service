5d666c2d55edb3368a681a3f51bc8bf9dea90273
Marcel Widmer <marzelwidmer@gmail.com>
2020-02-16 09:44:35 +0100

    Configurea SBB GracefullShutdown

--- src/main/kotlin/ch/keepcalm/demo/service/order/OrderServiceApplication.kt
+++ src/main/kotlin/ch/keepcalm/demo/service/order/OrderServiceApplication.kt
@@ -1,10 +1,10 @@
 package ch.keepcalm.demo.service.order
 
+import ch.sbb.esta.openshift.gracefullshutdown.GracefulshutdownSpringApplication
 import io.jaegertracing.internal.samplers.ConstSampler
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.info.BuildProperties
 import org.springframework.boot.info.GitProperties
-import org.springframework.boot.runApplication
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.client.loadbalancer.LoadBalanced
@@ -33,7 +33,7 @@ import java.util.*
 class OrderServiceApplication
 
 fun main(args: Array<String>) {
-    runApplication<OrderServiceApplication>(*args)
+    GracefulshutdownSpringApplication.run(OrderServiceApplication::class.java, *args)
 }
 
 @RestController
